59a791c3639b770da7e19583b85b970d22b2ad83
Umerfarooq-786 <shakil2232591@gmail.com>
2025-09-27 14:30:30 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Typed effect
+var typed = new Typed("#typed", {
+  strings: ["Umer Farooq", "Digital Marketer", "IT Specialist", "Cybersecurity Learner"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
+
+// AOS init
+AOS.init({
+  duration: 1200,
+  once: true
+});
